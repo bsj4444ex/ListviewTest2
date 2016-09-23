b48dfbba0496b0464374f304e35ef3a1e6957b39
bsj <bsj4444foxmail.com>
2016-09-23 16:57:22 +0800

    mainActivity

--- app/src/main/java/com/bsj4444/listviewtest2/MainActivity.java
+++ app/src/main/java/com/bsj4444/listviewtest2/MainActivity.java
@@ -2,6 +2,9 @@ package com.bsj4444.listviewtest2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -9,19 +12,61 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
+    private ListView2 listView;
     private List<String> list;
     ListViewAdapter listViewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView=(ListView)findViewById(R.id.listview);
+        listView=(ListView2)findViewById(R.id.listview);
         list=new ArrayList<String>();
         for(int i=0;i<10;i++){
             list.add(""+i);
         }
         listViewAdapter=new ListViewAdapter(this,list);
         listView.setAdapter(listViewAdapter);
+        //listView.setSelection(position);显示第几项,瞬间跳转
+//        listView.smoothScrollBy(distance,duration);
+//        listView.smoothScrollByOffset(offset);
+//        listView.smoothScrollToPosition(index);  实现平滑移动到指定的项
+        //listViewAdapter.notifyDataSetChanged(); 刷新动态修改后的listVIew中的内容
+        //View view=listView.getChildAt(position); 获取第position个itemView
+        //listView.setEmptyView(R.id.xxx);设置空数据时显示的内容
+        listView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_UP:
+                    break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                }
+                return false;
+            }
+        });
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                switch (scrollState){
+                    case SCROLL_STATE_IDLE://滑动停止时
+                        break;
+                    case SCROLL_STATE_TOUCH_SCROLL://正在滚动时
+                        break;
+                    case SCROLL_STATE_FLING://手指用力滑动时
+                        break;              //在离开后listView由于惯性继续滑动
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view,
+                                 int firstVisibleItem,
+                                 int visibleItemCount,
+                                 int totalItemCount) {
+                //滚动时一直调用
+            }
+        });
     }
 }
